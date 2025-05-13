7cd8ae0641192f5ef350eb3397aa7ec747d7e548
Abhishek Sunar <AbhishekSunar01>
2025-05-13 23:17:04 +0545

    Fix: Scrollable for main products container

--- apps/web/components/product-container.tsx
+++ apps/web/components/product-container.tsx
@@ -2,7 +2,7 @@ import { Product } from "./product";
 
 export default function ProductContainer() {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 w-full">
+    <div className="flex flex-col items-center justify-center gap-4 w-full mb-20">
       <Product />
       <Product />
       <Product />
